1928b9ca945702ef11ef25322be7910b935e2893
Daniel Agyapong <agyapongdaniel7777@gmail.com>
2022-10-30 15:24:08 -0700

    Update my_pearson_one_col_pred.py

--- Fall-2022/week-9/my_pearson_one_col_pred.py
+++ Fall-2022/week-9/my_pearson_one_col_pred.py
@@ -1,34 +1,44 @@
 import pandas as pd
 import numpy as np
 import plotnine as p9
-import os
+from datetime import date
+from sklearn.model_selection import KFold
 from sklearn.metrics import mean_absolute_error
+import sys, os
 
 # Name some string contants
-out_dir = "/scratch/da2343/cs685fall22/data"
-out_file = out_dir + "/pearson_model_one_col_results.csv"
-amgut_data_path = "./amgut1_data.csv"
-
+amgut_data_path = 'amgut1_data.csv'
 
 # Import the csv file of the amgut1_data
 amgut_data = pd.read_csv(amgut_data_path, header=0, index_col=0)
+# Remove the last column, fold_id, from the dataframe
+amgut_data = amgut_data.iloc[:, :-1]
+
 data_dict = {}
 
+n_row = amgut_data.shape[0]
 
-total_row_nos = amgut_data.shape[0]
-# should be in the range of 50s
-for index in range(1, total_row_nos, 24):
-    sub_data = amgut_data.iloc[:-index, :]
-    data_dict[f"amgut{index}"] = sub_data
-
+data_dict["amgut_0"] = amgut_data
+# should be in the range of 20s
+for index in range(1, n_row-20, 20):
+    sub_data_df = amgut_data.iloc[:-index, :]
+    data_dict[f"amgut{index}"] = sub_data_df
+    
+    
 test_acc_df_list = []
-for data_set, sub_data in data_dict.items():
-    cor_df = pd.DataFrame(columns=['taxa1', 'taxa2', 'cor'])
-    for i in range(0, len(sub_data.columns)):
-        for j in range(0, len(sub_data.columns)):
+for data_set_name, sub_data_df in data_dict.items():
+    cor_df = pd.DataFrame(columns=['taxa1', 'taxa2', 'cor', 'grad', 'intercept'])
+    n_col = len(sub_data_df.columns)
+    # ## Get cor_df from file system
+    # cor_df = pd.read_csv('./code/cor_df.csv')
+    # ## Filter it by the data_set_name column
+    # cor_df = cor_df[cor_df['dataset']==data_set_name]
+    
+    for i in range(n_col):
+        for j in range(n_col):
             if i != j:
-                taxa1_col = sub_data.iloc[:, i]
-                taxa2_col = sub_data.iloc[:, j]
+                taxa1_col = sub_data_df.iloc[:, i]
+                taxa2_col = sub_data_df.iloc[:, j]
                 calc_cor = taxa1_col.corr(taxa2_col, method='pearson')
                 abs_calc_cor = abs(calc_cor)
 
@@ -45,52 +55,53 @@ for data_set, sub_data in data_dict.items():
                 if abs_calc_cor >= 0:
                     # Append the taxa1, taxa2, cor to the cor_df
                     cor_df = pd.concat([cor_df,
-                                        pd.DataFrame({'taxa1': sub_data.columns[i],
-                                                      'taxa2': sub_data.columns[j],
+                                        pd.DataFrame({'taxa1': sub_data_df.columns[i],
+                                                      'taxa2': sub_data_df.columns[j],
                                                       'cor': abs_calc_cor,
                                                       'grad': grad,
                                                       'intercept': intercept}, index=[0])
                                         ], axis=0, ignore_index=True)
+ 
+    for index_col in range(125,126,125):
+        # Let X be all the columns/taxa except the dropped column/taxa of the sub_data dataframe
+         index_col = -1
+         y_actual = sub_data_df.iloc[:, index_col]
+         X_features = sub_data_df.drop(sub_data_df.columns[index_col], axis=1)
+        # get the name of y
+         y_name = sub_data_df.columns[index_col]
+        # Loop through all the columns in X
+         y_pred_list = []
+         for col_index in range(0, len(X_features.columns)):
+            # get the name of the column
+            x_name = X_features.columns[col_index]
+            # get the column
+            x_col = X_features.iloc[:, col_index]
+            # find the combination of x_name and y_name in the cor_df
+            # and get the gradient and intercept
+            grad = cor_df.loc[(cor_df['taxa1'] == x_name) & (
+                cor_df['taxa2'] == y_name), 'grad'].values[0]
+            intercept = cor_df.loc[(cor_df['taxa1'] == x_name) & (
+                cor_df['taxa2'] == y_name), 'intercept'].values[0]
+            y_pred = np.array((grad * x_col) + intercept)
+            y_pred_list.append(y_pred)
 
-    # Let y be the last column/taxa of the amgut1_data dataframe
-    y_actual = sub_data.iloc[:, -1]
-    # get the name of y
-    y_name = sub_data.columns[-1]
-    # Let x be all the columns/taxa except the last column/taxa of the amgut1_data dataframe
-    X = sub_data.iloc[:, :-1]
-
-    # Loop through all the columns in X
-    y_pred_list = []
-    for col_index in range(0, len(X.columns)):
-
-        # get the name of the column
-        x_name = X.columns[col_index]
-        # get the column
-        x_col = X.iloc[:, col_index]
-
-        # find the combination of x_name and y_name in the cor_df
-        # and get the gradient and intercept
-        grad = cor_df.loc[(cor_df['taxa1'] == x_name) & (
-            cor_df['taxa2'] == y_name), 'grad'].values[0]
-        intercept = cor_df.loc[(cor_df['taxa1'] == x_name) & (
-            cor_df['taxa2'] == y_name), 'intercept'].values[0]
-        y_pred = np.array((grad * x_col) + intercept)
-        y_pred_list.append(y_pred)
-        # print("Got Y pred here")
-
-    y_pred_mean = np.mean(y_pred_list, axis=0)
-    ma_error = mean_absolute_error(y_actual.to_numpy(), y_pred_mean)
-
-    test_acc_dict = {
-        "test_error": ma_error,
-        "# of Samples": sub_data.shape[0],
-        "algorithm": "Pearson Correlation"
-    }
-
-    test_acc_df_list.append(pd.DataFrame(test_acc_dict, index=[0]))
+            y_pred_mean = np.mean(y_pred_list, axis = 0)
+            ma_error = mean_absolute_error(y_actual.to_numpy(), y_pred_mean)
+            test_acc_dict = {
+                "test_error": ma_error,
+                "# of Samples": sub_data_df.shape[0],
+                "Index of Predicted Column": index_col,
+                "Algorithm": "Pearson Correlation",
+                "Data Set": "American Gut",
+            }
+            test_acc_df_list.append(pd.DataFrame(test_acc_dict, index=[0]))
+            
 test_acc_df = pd.concat(test_acc_df_list)
 
 
-# Save dataframe as a csv to output directory
-os.system("mkdir -p " + out_dir)
-test_acc_df.to_csv(out_file, encoding='utf-8', index=False)
+test_acc_df = pd.concat(test_acc_df_list)
+print(test_acc_df)
+gg = p9.ggplot(test_acc_df, p9.aes(x="# of Samples",
+                                     y="test_error" )) +\
+    p9.geom_line(color='red')
+gg.save("my_pearson_simple_facetted_1.png")
